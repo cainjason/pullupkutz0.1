add0b3791400f2c1836dda6986508ff5d163d3fb
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-26 22:25:36 -0400

    chore(fixed bug dependencies) @babel

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h2 className="text-3xl font-bold underline">Welcome to our app!</h2>
+    </div>
+  );
+}
+
+export default App;
