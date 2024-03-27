b466b70b1a10dd4e36e30f23e8e3240b73ea6c79
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-26 22:58:41 -0400

    chore(created branch) Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h2 className="text-3xl font-bold underline">Welcome to our app!</h2>
+      <Header />
     </div>
   );
 }
