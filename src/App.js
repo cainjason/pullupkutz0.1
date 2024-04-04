86cb7008ddbcde44c4395d9d4c3e5b0417b6552b
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-04-04 02:11:10 -0400

    chore(Sign in page)setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import SignInPage from "./components/SignInPage";
 
 function App() {
   return (
     <div className="app">
       <h2 className="text-3xl font-bold underline">Welcome to our app!</h2>
+      <SignInPage />
     </div>
   );
 }
