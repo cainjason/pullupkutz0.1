473b5cb85d69c9e716c0ec469cf132909c270987
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-04-07 23:57:36 -0400

    updated app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import SignIn from "./pages/SignIn";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
+      Let's build!
       <BrowserRouter>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/sign-in" element={<SignIn />} />
         </Routes>
       </BrowserRouter>
     </div>
