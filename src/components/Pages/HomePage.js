6b92dd86707e57d29faed1b66cfe99bae7daa426
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-04-07 22:23:32 -0400

    updated app

---
+++ src/components/Pages/HomePage.js
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <main>
+      <header>I'm am a header</header>
+      <body></body>
+      <footer></footer>
+    </main>
+  );
+}
+
+export default HomePage;
