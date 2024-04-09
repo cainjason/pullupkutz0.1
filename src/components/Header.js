057ddbf273934def1c32c543c17fea27da925aaa
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-04-09 13:30:14 -0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { FaHandPointRight } from "react-icons/fa";
 function Header() {
   return (
     <header className="flex justify-center bg-blue-600 h-12 p-1 w-full text-white font-black drop-shadow-md hover:drop-shadow-2xl ">
-      {<FaHandPointRight className="mt-2 mr-2 size-6" />}
+      <FaHandPointRight className="mt-2 mr-2 size-6" />
       <h2 className="hover: cursor-pointer text-3xl">Pullupkutz!</h2>
     </header>
   );
