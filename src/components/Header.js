0ef6287e15a70a7f4bb0263876c0465d2b7a09bb
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-04-09 13:21:07 -0400

    updated Home Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import { FaHandPointRight } from "react-icons/fa";
 
 function Header() {
   return (
-    <header className="flex justify-center bg-blue-600 h-8 w-full text-white font-black drop-shadow-md hover:drop-shadow-2xl ">
-      {<FaHandPointRight className="blink" />}
-      <h2 className="hover: cursor-pointer ">Pullupkutz!</h2>
+    <header className="flex justify-center bg-blue-600 h-12 p-1 w-full text-white font-black drop-shadow-md hover:drop-shadow-2xl ">
+      {<FaHandPointRight className="mt-2 mr-2 size-6" />}
+      <h2 className="hover: cursor-pointer text-3xl">Pullupkutz!</h2>
     </header>
   );
 }
